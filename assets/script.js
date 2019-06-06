50c6387732df9ce27bc1dead0bac80d93192bba9
AndyWams <andrew.wams@gmail.com>
2019-06-06 22:15:26 +0100

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $('.mobile-view-sec').click(function(){
+            $('.navigate-sec').toggleClass('active' );
+            $('.login-sec').toggleClass('active');
+        $('.drop-one').toggleClass('animated bounceInLeft');
+        $('.drop-two').toggleClass('animated bounceInLeft');
+        $('.drop-three').toggleClass('animated bounceInLeft');
+        $('.drop-four').toggleClass('animated bounceInLeft');
+    });
+});
